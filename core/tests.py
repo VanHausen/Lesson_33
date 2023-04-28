c0677199e5281507beda343d0347cbc933296822
tidus <tidussss@yandex.ru>
2023-04-28 12:24:16 +0300

    update_test_v1.0

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+'''from django.test import TestCase
 
-# Create your tests here.
+class TestCore1(TestCase):
+
+    def test_signup(self):
+        response = self.client.get('signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_password(self):
+        response = self.client.get('/update_password/')
+        self.assertEqual(response.status_code, 200)'''
--- goals/tests.py
+++ goals/tests.py
@@ -1,2 +1,51 @@
-from django.test import TestCase
+'''from django.test import TestCase
 
+class TestCore2(TestCase):
+
+    def test_board_create(self):
+        response = self.client.get('board/create')
+        self.assertEqual(response.status_code, 200)
+
+    def test_board_list(self):
+        response = self.client.get('board/list')
+        self.assertEqual(response.status_code, 200)
+
+    def test_board_pk(self):
+        response = self.client.get('board/<pk>')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_category_create(self):
+        response = self.client.get('goal_category/create')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_category_list(self):
+        response = self.client.get('goal_category/list')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_category_pk(self):
+        response = self.client.get('goal_category/<pk>')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_create(self):
+        response = self.client.get('goal/create')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_list(self):
+        response = self.client.get('goal/list')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_pk(self):
+        response = self.client.get('goal/<pk>')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_comment_create(self):
+        response = self.client.get('goal_comment/create')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_comment_list(self):
+        response = self.client.get('goal_comment/list')
+        self.assertEqual(response.status_code, 200)
+
+    def test_goal_comment_pk(self):
+        response = self.client.get('goal_comment/<pk>')
+        self.assertEqual(response.status_code, 200)'''
